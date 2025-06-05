1a466de479935ec277db0090a5ada1ecb7464824
fikre <fikre8186@gmail.com>
2025-06-05 20:43:20 +0300

    header and footer

---
+++ Netflix-clone-2025/src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Footer.css";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+function Footer() {
+  return (
+    <>
+      <footer className="netflix-footer">
+        <div className="footer-container">
+          <div className="footer-links">
+            <div className="colum">
+              <div className="social-links">
+                <a href="">
+                  <FacebookRoundedIcon />
+                </a>
+                <a href="">
+                  <InstagramIcon />
+                </a>
+                <a href="">
+                  <YouTubeIcon />
+                </a>
+              </div>
+              <a href="#">Aidio Description</a>
+              <a href="#">Investor Relations</a>
+              <a href="#">Legal Notice</a>
+              <a href="#">Service Code</a>
+            </div>
+            <div className="colum">
+              <a href="#">Help Center</a>
+              <a href="#">Jobs</a>
+              <a href="#">Cookie Prefernce</a>
+            </div>
+            <div className="colum">
+              <a href="#">Gift Cards</a>
+              <a href="#">Terms of Use</a>
+              <a href="#">Corporate Information</a>
+            </div>
+            <div className="colum">
+              <a href="#">Media Center</a>
+              <a href="#">Privacy</a>
+              <a href="">Contact Us</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
---
+++ Netflix-clone-2025/src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+function Header() {
+  return (
+    <>
+      <header class="header">
+        <div className="left-header">
+          <a href="#" class="logo">
+            NETFLIX
+          </a>
+          <nav class="nav-links">
+            <li>
+              {" "}
+              <a href="#">Home</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#">TV Shows</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#">Latest</a>
+            </li>
+            <li>
+              <a href="#">My List</a>
+            </li>
+            <li>
+              <a href="#">Browse by language</a>
+            </li>
+            <li>
+              <a href="#">NetFlix</a>
+            </li>
+          </nav>
+        </div>
+        <div className="right-header">
+          <li>
+            <a href="">
+              <SearchIcon />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <CircleNotificationsIcon />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <AccountBoxIcon />{" "}
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <ArrowDropDownIcon />{" "}
+            </a>
+          </li>
+        </div>
+      </header>
+    </>
+  );
+}
+export default Header;
